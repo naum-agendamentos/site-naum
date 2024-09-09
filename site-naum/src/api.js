be421787897e8999bbde3d4bd28170ceba371e56
Bryan Silva <125322115+BryanSilva117@users.noreply.github.com>
2024-09-09 10:45:06 -0300

    Update api.js

--- site-naum/src/api.js
+++ site-naum/src/api.js
@@ -4,7 +4,7 @@ import axios from "axios";
 // Cria uma instância do Axios com configurações personalizadas.
 const api = axios.create({
     
-    baseURL: "http://44.201.253.244:8080/"
+    baseURL: "http://10.0.0.165:8080/"
 });
     
 // Exporta a instância criada para que possa ser utilizada em outras partes do projeto.
